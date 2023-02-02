a7915e4470b2dab6c8b4c16efc8f38679af196d9
russmc <digitalbreadllc@gmail.com>
2023-02-02 09:23:24 -0700

    integrate RR and Dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/OdoMec.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/OdoMec.java
@@ -209,48 +209,11 @@ public class OdoMec extends LinearOpMode {
             double motorVerticalLiftRightPower = -gamepad2.left_stick_y;
             double motorVerticalLiftLeftPower = -gamepad2.left_stick_y;
 
-            if (!slideMag.isPressed()) {
-                if (gamepad2.dpad_up) { // Lift to top cone stack
-                    clawLinkage.setPosition(CLAW_LINKAGE_FIVE); // 0.53 - 5 cones, ALl the way up
-                    lift(75,1);
-
-                } else if (gamepad2.dpad_left) { // Lift to small junction
-                    lift(240,1);
-                } else if (gamepad2.dpad_down) { // Lift to med junction
-                    lift(535,1);
-                }  else if (gamepad2.dpad_right) { // Lift to tall junction
-                    lift(980,1);
-                } else {
-                    // SLIDE_UP with left joy stick
-                    motorVerticalLiftRightPower = Range.clip(motorVerticalLiftRightPower, -0.6, 1); // was 1
-                    motorVerticalLiftLeftPower = Range.clip(motorVerticalLiftLeftPower, -0.6, 1); //-0.90, .93)
-                    rightSlide.setPower(motorVerticalLiftRightPower);
-                    leftSlide.setPower(motorVerticalLiftLeftPower);
-                }
-            } else { // SLIDE_DOWN
-                motorVerticalLiftRightPower = Range.clip(motorVerticalLiftRightPower, 0, 1);// 1
-                motorVerticalLiftLeftPower = Range.clip(motorVerticalLiftLeftPower, 0, 1);// .93
+            if (!slideMag.isPressed()) { // SLIDE_UP
+                motorVerticalLiftRightPower = Range.clip(motorVerticalLiftRightPower, -0.6, 1); // was 1
+                motorVerticalLiftLeftPower = Range.clip(motorVerticalLiftLeftPower, -0.6, 1); //-0.90, .93)
                 rightSlide.setPower(motorVerticalLiftRightPower);
                 leftSlide.setPower(motorVerticalLiftLeftPower);
-            }
-            if (!slideMag.isPressed()) {
-                if (gamepad2.dpad_up) { // Lift to top cone stack
-                    clawLinkage.setPosition(CLAW_LINKAGE_FIVE); // 0.53 - 5 cones, ALl the way up
-                    lift(75,1);
-
-                } else if (gamepad2.dpad_left) { // Lift to small junction
-                    lift(240,1);
-                } else if (gamepad2.dpad_down) { // Lift to med junction
-                    lift(535,1);
-                }  else if (gamepad2.dpad_right) { // Lift to tall junction
-                    lift(980,1);
-                } else {
-                    // SLIDE_UP with left joy stick
-                    motorVerticalLiftRightPower = Range.clip(motorVerticalLiftRightPower, -0.6, 1); // was 1
-                    motorVerticalLiftLeftPower = Range.clip(motorVerticalLiftLeftPower, -0.6, 1); //-0.90, .93)
-                    rightSlide.setPower(motorVerticalLiftRightPower);
-                    leftSlide.setPower(motorVerticalLiftLeftPower);
-                }
             } else { // SLIDE_DOWN
                 motorVerticalLiftRightPower = Range.clip(motorVerticalLiftRightPower, 0, 1);// 1
                 motorVerticalLiftLeftPower = Range.clip(motorVerticalLiftLeftPower, 0, 1);// .93
